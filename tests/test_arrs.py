b253e5a61b685d3b2777c4df729b101c5216af40
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-05-20 12:53:25 +0400

    new text in file test_arrs,new docstring, test coverage 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -10,6 +10,7 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    """new tests"""
     assert arrs.my_slice([], 1) == []
     assert arrs.my_slice([1, 2, 3], -1) == [3]
     assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
